7d7e329bb99ee71b81cb05fc77a6f0b58f631e7d
prince <princeahirwar51@gmail.com>
2022-03-29 14:14:19 +0530

    second v2

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 // atlas mongo
-const mon = "mongodb+srv://kady1411:Kadyan@cluster0.ft1d1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const mon = "mongodb+srv://kady1411:Kadyan@cluster0.ft1d1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const mon = "mongodb://Quantiphi123:Quantiphi123@mock-project-database.cja5akqsen0c.us-west-2.docdb.amazonaws.com:27017/mock-project-database?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
 mongoose
   .connect(mon, {
     useNewUrlParser: true,
